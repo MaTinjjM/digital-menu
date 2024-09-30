0ac9c9e2602cc821a4f5e15ed9779acf20cf57bf
matin madah <matinjjt@gmail.com>
2024-09-30 05:25:51 +0330

    V-0.2

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,32 @@ function App() {
   return (
     <>
         <body>
-    <div id='Main' className="bg-[#232323] font-IYME">
+    <div id='Main' dir='rtl' className="bg-[#232323] font-IYME">
         <div className="flex items-center justify-center h-screen">
-            <div className="h-full w-full lg:w-1/3 bg-[#be9999]">
+            <div className="h-full w-full lg:w-1/3 bg-[#232323]">
                <div id='header' className='mt-2 mx-14 px-5'>
-                    <div className="flex justify-between mt-2 content-normal	">
+                    <div id='T-H' className="flex justify-between mt-2 content-normal	">
                         <h1 className="order-2 text-xl text-center aviny text-white">نیلا فود</h1>
-                        <img className='order-1 ' src="http://menutik.ir/panel/storeImage/18/18_1287461600.png" alt='Error' width={35} height={32}></img>
-                        <FontAwesomeIcon icon={faCartShopping} className='order-3 w-35 h-35 p-2 text-white hover:text-black border rounded-full border-white' />
+                        <button onClick={"#"} className='order-1'>
+                          <FontAwesomeIcon icon={faCartShopping} onClick={'#'} className='button w-4 h-4 p-2 text-white bg-[#4D4D4D] hover:text-black border rounded-full border-[#4D4D4D]' />
+                        </button>
+                        <button className='order-3 w-10 h-10' onClick="#">
+                           <img src="http://menutik.ir/panel/storeImage/18/18_1287461600.png" />
+                        </button>
+                    </div>
+                    <div className='mt-4 flex flex-nowrap items-center justify-between'>
+                       <a href='#' target='_blank' className='btn flex items-center gap-2 py-1 pl-4 rounded-3xl bg-gradient-to-r from-[#2A2A2A] via-[#808080] to-[#ffffff]'>
+                         <span>
+                           <img src='https://mt2.ir/assets/images/instagramlogo.png' className='me-2 w-5 h-8 rounded '></img>
+                         </span>
+                         صفحه اینستاگرام
+                       </a> 
+                       <a href='#' target='_blank' className='btn flex items-center gap-2 py-1 pl-4 rounded-3xl bg-gradient-to-r from-[#2A2A2A] via-[#808080] to-[#ffffff]'>
+                         <span>
+                           <img src='https://mt2.ir/assets/images/phone.png' className='me-2 w-5 h-8 rounded '></img>
+                         </span>
+                         09026718543
+                       </a> 
                     </div>
                 </div> 
             </div>
