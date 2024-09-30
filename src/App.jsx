52339213bf1ce7b3940b4e90a1f91ff538a84d94
matin madah <matinjjt@gmail.com>
2024-09-30 04:19:02 +0330

    V-0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <body>
+    <div id='Main' className="bg-[#232323] font-IYME">
+        <div className="flex items-center justify-center h-screen">
+            <div className="h-full w-full lg:w-1/3 bg-[#be9999]">
+               <div id='header' className='mt-2 mx-14 px-5'>
+                    <div className="flex justify-between mt-2 content-normal	">
+                        <h1 className="order-2 text-xl text-center aviny text-white">نیلا فود</h1>
+                        <img className='order-1 ' src="http://menutik.ir/panel/storeImage/18/18_1287461600.png" alt='Error' width={35} height={32}></img>
+                        <FontAwesomeIcon icon={faCartShopping} className='order-3 w-35 h-35 p-2 text-white hover:text-black border rounded-full border-white' />
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</body>
     </>
   )
 }
